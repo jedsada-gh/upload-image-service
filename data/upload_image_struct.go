dc6bc15f8cf7fc4a51a7712ee85c853b6187704e
Jedsada Tiwongvorakul <jt@20scoops.net>
2018-02-06 14:34:58 +0700

    create struct image upload

---
+++ data/upload_image_struct.go
@@ -0,0 +1,13 @@
+package data
+
+import (
+	"mime/multipart"
+)
+
+// UploadImage is exported because it starts with a capital letter
+type UploadImage struct {
+	Bucket string
+	APIKey string
+	Region string
+	Image  multipart.File
+}
--- mian.go
+++ mian.go
@@ -4,11 +4,11 @@ import (
 	"bytes"
 	"fmt"
 	"io"
-	"mime/multipart"
 	"net/http"
 	"os"
 
 	"github.com/gorilla/handlers"
+	"github.com/upload-image-service/data"
 	filetype "gopkg.in/h2non/filetype.v1"
 )
 
@@ -16,18 +16,23 @@ var (
 	httpPort      = os.Getenv("PORT")
 	httpPortLocal = "8080"
 	listenIP      = "localhost"
-	s3Region      = ""
-	s3Bucket      = ""
 )
 
 var (
-	messageMethodNotAllowed = "Method Not Allowed"
-	messageFileNotSupported = "File Not Support bacause type Image only"
+	messageMethodNotAllowed  = "Method Not Allowed"
+	messageFileNotSupported  = "File Not Support bacause type Image only"
+	messageBucketNameInvalid = "Bucket Invalid"
+	messageAPIKeyInvalid     = "API Key Invalid"
+	messageRegionInvalid     = "Region Invalid"
+	messageNoSuchFile        = "No such file"
 )
 
 const (
 	methodPost = "POST"
 	keyImage   = "file"
+	keyBucket  = "bucket"
+	keyRegion  = "region"
+	keyAPIKey  = "api_key"
 )
 
 const (
@@ -44,28 +49,49 @@ func main() {
 
 func handlerUpload(w http.ResponseWriter, r *http.Request) {
 	if r.Method == methodPost {
-		file, _, err := r.FormFile(keyImage)
-		if err != nil {
-			errorMessage(w, http.StatusBadRequest, err.Error())
-			return
-		}
-		defer file.Close()
-		validateTypeFile(file, w, r)
+		validateValue(w, r)
 	} else {
 		errorMessage(w, http.StatusMethodNotAllowed, messageMethodNotAllowed)
 	}
 }
 
-func validateTypeFile(file multipart.File, w http.ResponseWriter, r *http.Request) {
+func validateValue(w http.ResponseWriter, r *http.Request) {
+	bucket := r.FormValue(keyBucket)
+	apiKey := r.FormValue(keyAPIKey)
+	region := r.FormValue(keyRegion)
+	if len(bucket) == 0 {
+		errorMessage(w, http.StatusBadRequest, messageBucketNameInvalid)
+	} else if len(apiKey) == 0 {
+		errorMessage(w, http.StatusBadRequest, messageAPIKeyInvalid)
+	} else if len(region) == 0 {
+		errorMessage(w, http.StatusBadRequest, messageRegionInvalid)
+	} else {
+		fmt.Printf("%s\t%s\t%s\n", bucket, apiKey, region)
+		file, _, err := r.FormFile(keyImage)
+		if err != nil {
+			errorMessage(w, http.StatusBadRequest, messageNoSuchFile)
+		} else {
+			defer file.Close()
+			var model data.UploadImage
+			model.APIKey = apiKey
+			model.Bucket = bucket
+			model.Region = region
+			model.Image = file
+			validateTypeFile(model, w, r)
+		}
+	}
+}
+
+func validateTypeFile(model data.UploadImage, w http.ResponseWriter, r *http.Request) {
 	buf := bytes.NewBuffer(nil)
-	_, error := io.Copy(buf, file)
+	_, error := io.Copy(buf, model.Image)
 	if error != nil {
 		errorMessage(w, http.StatusBadRequest, error.Error())
 	} else if buf == nil {
 		errorMessage(w, http.StatusBadRequest, messageFileNotSupported)
 	} else if filetype.IsImage(buf.Bytes()) {
 		// TODO : upload file to aws s3
-		w.Write([]byte(fmt.Sprintf("%d bytes are recieved.\n", file)))
+		w.Write([]byte(fmt.Sprintf("%d bytes are recieved.\n", model.Image)))
 	} else {
 		errorMessage(w, http.StatusBadRequest, messageFileNotSupported)
 	}
