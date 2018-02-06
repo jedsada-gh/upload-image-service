7463577f0035d2e0dca2e8473f91f0b7282f8d79
Jedsada Tiwongvorakul <jt@20scoops.net>
2018-02-06 18:51:00 +0700

    handler error to json

--- util/error_message.go
+++ util/error_message.go
@@ -1,12 +1,24 @@
 package util
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"github.com/upload-image-service/data"
 )
 
 // ErrorMessage is response message error request
 func ErrorMessage(w http.ResponseWriter, statusCode int, message string) {
+	var errorModel data.Error
+	var errorDetailModel data.ErrorDetail
+	errorDetailModel.Message = message
+	errorModel.ErrorDetail = errorDetailModel
+	movie, err := json.Marshal(errorModel)
+	if err != nil {
+		fmt.Println(err)
+	}
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(statusCode)
-	w.Write([]byte(fmt.Sprint(message)))
+	w.Write(movie)
 }
